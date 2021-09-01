724a454ce7854913f3a5d8bb7e0b009c23a5a4a2
Tema-front <polsowatel.artem@gmail.com>
2021-09-01 21:30:52 +0300

    second lesson is ready

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-// import logo from './logo.svg';
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Message } from './components/Message';
 
-const warningTxt = 'Внимание, вы не авторизованы. Опции ограниченны'
-
 function App() {
-  return (
-    <div className="App">
 
-      <Message text={warningTxt}/>
+  const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState('');
+  const answerBot = [
+    'Hello, i\'m a bot',
+    'I\'m written on React', 
+    'Leave me alone', 'Get away!', 
+    'Finally, you have done this task',
+    'Tema wrote this homework for two days >:|'
+  ];
+
+  // Неожиданный для меня эффект получился благодаря этой записи
+  // Если таймаута не записывать в эту переменную и нажать много раз кнопку отправки сообщения,
+  // То в стеке вызовов будет много таймаутов
+  // Но всё исправляет запись интервала в перменную
+  // Правда, появился какой-то варн, но это издержки профессии)
+  // В продакшен!
+  let answerTimeout = undefined
+
+  useEffect(() => {
+    if (messageList[messageList.length - 1]?.author === 'User') {
+      // Таймер для ответа бота с генерацией случайного ответа из массива ответов)
+      answerTimeout = setTimeout(() => {
+        setMessageList((prevMessages) => [...prevMessages, { text: answerBot[Math.floor(Math.random() * (6 - 0)) + 0], author: 'Bot'}])
+      }, 1500);
+    }
+    return () => {
+      clearTimeout(answerTimeout)
+    }
+  }, [messageList, answerBot])
+
+  const handleText = (event) => { 
+    setMessage(event.target.value)
+  }
+
+  const saveMessages = (event) => {
+    event.preventDefault()
+    // Отчищаю поле инпута при отправке формы
+    setMessage('')
+  }
+  
+  const handleClick = () => {
+    // При клике добавляю в массив новый объект сообщения
+    setMessageList(prevMessageList => [...prevMessageList, {text: message, author: 'User'}])
+  }
+
+
+  return (
+    <div className="App warningBlock"> 
+      <div className="wrapperForm">
+
+        <form  className="formBlock" onSubmit={saveMessages}>
+          <input className="inputMessage" onChange={handleText} value={message}></input>
+          <button className="btnSendMessage" onClick={handleClick}>SEND</button>
+        </form>
+
+        <div className="textSpace">
+          {messageList.map((message, i) => <Message message={message} key={i}/>)}
+        </div>
+        
+      </div>
     </div>
   );
 }
-// const message = 'This is first task with react course';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Message mess={ message }/>
-//   </React.StrictMode>,
-//   document.getElementsByClassName('App')
-// );
-
-
-// Message(message)
 
 export default App;
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
 import '../../App.css'
 
-let counter = 60
+export const Message = (props) => {
 
-export const Message = ({text}) => {
     return (
-        <div className="warningBlock">
-            <h1>{ text }</h1>
-            <p>Покиньте страницу в течение {counter} секунд</p>
-        </div>
+        <p className="message"> 
+            {props.message.author}: {props.message.text}
+        </p>
     )
 };  
